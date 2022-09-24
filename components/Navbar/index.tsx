496eaad89bbbdb89005b63cb522a9a48505ed872
Etem Senel <etemsenel96@hotmail.de>
2022-09-24 11:15:21 +0300

    icons added.

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full h-20 bg-red-200">
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>National Team</li>
+          <li>Premier Leauge</li>
+          <li>Bundesliga</li>
+          <li>Seria A</li>
+          <li>La Liga</li>
+          <li>Ligue One</li>
+          <li>Other Clubs</li>
+        </ul>
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+export default Navbar;
