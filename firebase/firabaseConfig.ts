2ed1b94724fa4d10264698ed4eb9f7b81612d619
Etem Senel <etemsenel96@hotmail.de>
2022-09-25 05:11:16 +0300

    firebase settings completed.

---
+++ firebase/firabaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_DATABASE_URL,
+  projectId: process.env.NEXT_PROJECT_ID,
+  storageBucket: process.env.NEXT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_ID,
+};
+
+//initilizae firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
