ae95beaebe1279c452a2bbd46ed8210056d9414a
Etem Senel <etemsenel96@hotmail.de>
2022-09-25 11:12:18 +0300

    Product interface added.

---
+++ interfaces/ProductInterface.ts
@@ -0,0 +1,14 @@
+export interface TshirtProps {
+  backLarge: string;
+  backSmall: string;
+  description: string[];
+  frontLarge: string;
+  frontSmall: string;
+  isBestSeller: boolean;
+  isStock: boolean;
+  leuage: string;
+  price: number;
+  tshirtName: string;
+  uid: string;
+  id: string;
+}
