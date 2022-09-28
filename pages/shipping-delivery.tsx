fafd9776cfbd4c8dfd15a9e46f4e6e42efd8107f
Etem Senel <etemsenel96@hotmail.de>
2022-09-28 11:43:36 +0300

    shipping-delivery component added.

---
+++ pages/shipping-delivery.tsx
@@ -0,0 +1,46 @@
+const shippingDelivery = () => {
+  return (
+    <div className="mt-14 min-h-[50vh]">
+      <h1 className="text-border text-2xl font-bold text-zinc-700 text-center">
+        Shipping Policy
+      </h1>
+      <div className="px-8 py-8 w-11/12 mx-auto max-w-3xl">
+        <p className="py-4 text-sm leading-7 text-zinc-700">
+          We are proud to offer international shipping services that currently
+          operate in over 200 countries and islands world wide. Nothing means
+          more to us than bringing our customers great value and service. We
+          will continue to grow to meet the needs of all our customers,
+          delivering a service beyond all expectation anywhere in the world.
+        </p>
+        <h4 className="font-bold pt-4">Shipping Time</h4>
+        <p className="py-4 text-sm leading-7 text-zinc-700">
+          International mail: 10-40 days (Not including our proccess time is
+          about 1-3 working days) Fedex/UPS/DHL: 5-15 days (Not including our
+          proccess time is about 1-3 working days)
+        </p>
+        <p className="py-2 text-sm leading-7 text-zinc-700">
+          COVID-19 shipping delays - Please be aware shipping times are taking
+          longer due to the global pandemic. We apologize but the delays are out
+          of our control.
+        </p>
+        <h4 className="font-bold pt-4">Tax Charges</h4>
+        <p className="text-sm leading-7 text-zinc-700">
+          Customs can often charge duty on imports into certain countries. We
+          have no control over this and cannot be held responsible for any
+          import taxes/duties that may be charged. These taxes go to the
+          government of the country that you are importing the goods. You will
+          be liable for any charges incurred due to import taxes. It is illegal
+          to undervalue/mark as a gift any parcel to avoid paying duty, so
+          please don’t ask us to do this, as any requests will be ignored.
+        </p>
+
+        <h4 className="font-bold pt-4">Contact Us</h4>
+        <p className="text-sm leading-7 text-zinc-700">
+          If you have any other questions, please contact us and we will do our
+          best to help you out.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default shippingDelivery;
