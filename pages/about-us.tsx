aee60737193f4790f59e59902c0c29ab6cf71cf3
Etem Senel <etemsenel96@hotmail.de>
2022-09-28 08:46:11 +0300

    about me component added.

---
+++ pages/about-us.tsx
@@ -0,0 +1,43 @@
+const aboutUs = () => {
+  return (
+    <div className="mt-6 min-h-[50vh] text-center">
+      <h1 className="text-border text-2xl font-bold">ABOUT US</h1>
+      <div className="px-8 mt-4 py-4 w-11/12 mx-auto md:w-8/12 lg:w-6/12">
+        <p className="py-4 text-sm leading-7">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+          ipsa magnam, quas iste, neque beatae labore exercitationem animi
+          inventore consectetur minima quos nihil qui maxime!
+        </p>
+        <p className="py-4 text-sm leading-7">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
+          expedita minus distinctio eius rerum harum nemo reprehenderit amet
+          libero, minima fugiat voluptatum eos deserunt qui natus, fuga at,
+          blanditiis delectus ad impedit obcaecati saepe. Doloribus eos
+          assumenda dolores impedit unde.
+        </p>
+        <p className="py-4 text-sm leading-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          consequatur vitae, accusamus exercitationem quam quasi earum veniam
+          illo ad, harum provident cumque dolorem! Aut totam a, beatae sit
+          quaerat at aliquam, vitae illum necessitatibus tempore minus
+          veritatis! Saepe accusantium natus consequatur quaerat mollitia, rem
+          sequi! Illo cumque nulla temporibus quibusdam odit, itaque veritatis,
+          commodi blanditiis maiores dignissimos cum! Iusto, neque.
+        </p>
+        <p className="py-4 text-sm leading-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          eveniet enim voluptates esse dignissimos, accusamus reprehenderit
+          eligendi rem obcaecati velit officiis sapiente repellat consequatur
+          deleniti nulla ut, tempore repudiandae libero! Nisi corrupti
+          temporibus distinctio unde eum soluta rem autem quia.
+        </p>
+        <p className="py-4 text-sm leading-7">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
+          voluptates exercitationem accusamus necessitatibus. Ipsam, saepe
+          autem? Quod neque velit quis.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default aboutUs;
