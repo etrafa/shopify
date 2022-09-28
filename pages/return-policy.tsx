1372aef04dc242c19aebc51a95eeee6d6b3414eb
Etem Senel <etemsenel96@hotmail.de>
2022-09-28 12:19:17 +0300

    return policy component added.

---
+++ pages/return-policy.tsx
@@ -0,0 +1,92 @@
+const ReturnPolicy = () => {
+  return (
+    <div className="mt-14 min-h-[50vh]">
+      <h1 className="text-border text-2xl font-bold text-zinc-700 text-center">
+        Return Policy
+      </h1>
+      <div className="px-8 py-8 w-11/12 mx-auto max-w-3xl">
+        <h4 className="font-bold pt-4">Order cancellation</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          All orders can be cancelled until they are shipped. If your order has
+          been paid and you need to make a change or cancel an order, you must
+          contact us within 12 hours. Once the packaging and shipping process
+          has started, it can no longer be cancelled.
+        </p>
+
+        <p className="py-2 text-sm leading-7 text-zinc-700">
+          Considering the transportation time of the return takes more than 1
+          month also the expensive shipping fee, {"what's"} more We want to
+          establish a long-term cooperative relationship with you, so here is
+          our win-win solution.
+        </p>
+
+        <h4 className="font-bold pt-4">WHAT IS OUR RESENT POLICY WORKING?</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If you are with one of the following conditions, after we confirm, we
+          will resent you a new package.
+        </p>
+
+        <h4 className="font-bold pt-4">1.SIZE PROBLEM</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If you think the items that you received are not fit for you, Provide
+          pictures that you wearing the jersey for us to confirm, After we
+          confirm you can pay $15 postage for resending, we will arrange a new
+          shipment for you.
+        </p>
+
+        <h4 className="font-bold pt-4">2. DELAY SHIPMENT</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If the shipment time is more than 60 days, and it has been more than
+          60 days since you could track the package online, please contact us,
+          and then we will resend you the package at once for free.
+        </p>
+
+        <h4 className="font-bold pt-4">3. CUSTOMS HOLD OR LOST</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If the package is getting lost or seized by the customs. please
+          provide us proof about that, and we will resend the package to you for
+          free after confirm, and make sure you get it.
+        </p>
+
+        <h4 className="font-bold pt-4">Exchanges</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If for any reason you would like to exchange your product, perhaps for
+          a different size in clothing. You must contact us first and we will
+          guide you through the steps.
+        </p>
+        <h4 className="font-bold pt-4">
+          In this case, please kindly provide us with proofs as below:
+        </h4>
+
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          -The photos of the original package with front and back sides -The
+          photos or video of the defective item(s) -The photos, video or
+          screenshot showing the mal-function of the defective item(s)
+        </p>
+        <h4 className="font-bold pt-4">
+          After returning the defective product, please kindly provide us with
+          proofs as below:
+        </h4>
+
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          -Photos of the front and back of the packaging envelope or box
+          -Partial shipment slips (if applicable) -Photo, video, screenshot, or
+          description of the defective item -Photo, video, screenshot, or
+          description of the defect or defective area
+        </p>
+        <h4 className="font-bold pt-4">Return shipping method</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          For the replacement, we will send it out in the shipment ways as the
+          customer selected in their original order.
+        </p>
+
+        <h4 className="font-bold pt-4">Contact us</h4>
+        <p className="pt-4 pb-2 text-sm leading-7 text-zinc-700">
+          If you have any other questions, please contact us and we will do our
+          best to help you out.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default ReturnPolicy;
