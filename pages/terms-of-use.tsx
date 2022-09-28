d4d4be9616568e9d177cfac32e0086af10626dd4
Etem Senel <etemsenel96@hotmail.de>
2022-09-28 08:56:26 +0300

    terms-of-use & privacy&policy components added.

--- pages/terms-of-use.tsx
+++ pages/terms-of-use.tsx
@@ -1,7 +1,9 @@
-const aboutUs = () => {
+const TermsofUse = () => {
   return (
     <div className="mt-14 min-h-[50vh] text-center">
-      <h1 className="text-border text-2xl font-bold text-zinc-700">About Us</h1>
+      <h1 className="text-border text-2xl font-bold text-zinc-700">
+        Terms of Use
+      </h1>
       <div className="px-8 mt-4 py-4 w-11/12 mx-auto md:w-8/12 lg:w-6/12">
         <p className="pt-2 pb-4 text-sm leading-7 text-zinc-700">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
@@ -40,4 +42,4 @@ const aboutUs = () => {
     </div>
   );
 };
-export default aboutUs;
+export default TermsofUse;
