4ba68f68b3bef9c92cec1718bcaaf385b931596d
Etem Senel <etemsenel96@hotmail.de>
2022-10-03 08:28:00 +0300

    card component thead created.

--- components/Footer/FooterItems_FOR_USERS.tsx
+++ components/Footer/FooterItems_FOR_USERS.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 const FooterItems_FOR_USERS = () => {
   return (
     <ul className="mt-3">
-      <Link href="/account/my-account/order-history">
+      <Link href="/account/order-history">
         <li className="cursor-pointer hover:underline">My Orders</li>
       </Link>
-      <Link href="/account/my-account/wishlist">
+      <Link href="/account/wishlist">
         <li className="mt-2 cursor-pointer hover:underline">My Wishlist</li>
       </Link>
-      <Link href="/account/my-account/cart">
+      <Link href="/account/cart">
         <li className="mt-2 cursor-pointer hover:underline">My Cart</li>
       </Link>
-      <Link href="/account/my-account/profile">
+      <Link href="/account/profile">
         <li className="mt-2 cursor-pointer hover:underline">My Profile</li>
       </Link>
     </ul>
--- pages/account/cart.tsx
+++ pages/account/cart.tsx
@@ -1,4 +1,54 @@
 const Cart = () => {
-  return <div></div>;
+  return (
+    <div className="w-full mt-14 min-h-[50vh]">
+      <header>
+        <h1 className="text-border text-2xl font-bold text-zinc-700 text-center">
+          Your Cart
+        </h1>
+      </header>
+      <form className="mt-12">
+        <table className="w-full text-left max-w-screen-lg lg:mx-auto">
+          <thead>
+            <tr className=" text-button-text text-[8px] tracking-widest uppercase">
+              <th className="py-3 px-6">Product name</th>
+              <th className="py-3 px-6 hidden md:block">Quantity</th>
+              <th className="py-3 px-6 text-right">Total</th>
+            </tr>
+          </thead>
+          {/* <tbody>
+            <tr className="bg-white border-b ">
+              <th
+                scope="row"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
+              >
+                Apple MacBook Pro 17
+              </th>
+              <td className="py-4 px-6">$2999</td>
+            </tr>
+            <tr className="bg-white border-b ">
+              <th
+                scope="row"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
+              >
+                Microsoft Surface Pro
+              </th>
+
+              <td className="py-4 px-6">$1999</td>
+            </tr>
+            <tr className="bg-white ">
+              <th
+                scope="row"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
+              >
+                Magic Mouse 2
+              </th>
+
+              <td className="py-4 px-6">$99</td>
+            </tr>
+          </tbody> */}
+        </table>
+      </form>
+    </div>
+  );
 };
 export default Cart;
