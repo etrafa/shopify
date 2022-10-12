710d60db3578b13a41e14d8a534426a410009bf8
Etem Senel <etemsenel96@hotmail.de>
2022-10-12 06:45:13 +0300

    cart modal created.

---
+++ components/Layouts/SingleProduct/AddItemToCartModal.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ISingleProductForCart } from "../../../interfaces/SingleProductForCart";
+import { CloseIcon } from "../../../Utilities/Icons/Icons";
+
+interface AddItemToCartModalProps {
+  tshirtName: string;
+  frontLarge: string;
+  size: string;
+}
+
+const AddItemToCartModal = (props: AddItemToCartModalProps) => {
+  return (
+    <div className="w-full min-h-screen bg-white z-50 rounded-md absolute left-1/2 transform -translate-x-1/2 -top-10">
+      <p className="text-sm py-10 pl-24 opacity-80">
+        <span className="text-xs">✔️</span> Item added to your cart
+      </p>
+      <CloseIcon iconStyle="w-6 h-6 absolute top-2 right-2 opacity-60 cursor-pointer hover:scale-110" />
+      <div className="flex">
+        <span className="pt-6 pl-4">
+          <Image
+            src={props.frontLarge}
+            alt={props.tshirtName}
+            width={120}
+            height={120}
+          />
+        </span>
+        <div className="px-4">
+          <h2 className="text-xs opacity-80 mt-6">{props.tshirtName}</h2>
+          <p className="text-sm opacity-80 py-2">Size: {props.size}</p>
+        </div>
+      </div>
+      <div>
+        <Link href="/account/cart">
+          <button className="mt-12 w-11/12 h-12 block mx-auto border rounded-sm bg-light-gray text-sm tracking-widest hover:scale-105 ease-in-out text-button-text">
+            Go to cart
+          </button>
+        </Link>
+        <p className="underline underline-offset-2 text-center mt-4 cursor-pointer">
+          Continue shopping
+        </p>
+      </div>
+    </div>
+  );
+};
+export default AddItemToCartModal;
--- components/Layouts/SingleProduct/SingleProduct.tsx
+++ components/Layouts/SingleProduct/SingleProduct.tsx
@@ -1,5 +1,6 @@
 import { IProduct } from "../../../interfaces/ProductInterface";
 import { useAppSelector } from "../../../src/store";
+import AddItemToCartModal from "./AddItemToCartModal";
 import SingleProductButton from "./SingleProductButton";
 import SingleProductDetails from "./SingleProductDetails";
 import SingleProductHeader from "./SingleProductHeader";
@@ -11,32 +12,39 @@ const SingleProduct = (props: IProduct) => {
   const { value } = useAppSelector((store) => store.product);
 
   return (
-    <div className="md:flex md:justify-center">
-      <SingleProductPreviews
-        backLarge={props.backLarge}
-        backSmall={props.backSmall}
+    <div className="relative">
+      <div className="md:flex md:justify-center">
+        <SingleProductPreviews
+          backLarge={props.backLarge}
+          backSmall={props.backSmall}
+          frontLarge={props.frontLarge}
+          frontSmall={props.frontSmall}
+          tshirtName={props.tshirtName}
+        />
+        <div className="md:mt-16 lg:mx-12 max-w-sm">
+          <SingleProductHeader
+            tshirtName={props.tshirtName}
+            price={props.price}
+          />
+          <SingleProductSize />
+          <SingleProductQuantity />
+          <SingleProductButton
+            id={props.id}
+            tshirtName={props.tshirtName}
+            leauge={props.leauge}
+            price={props.price}
+            frontLarge={props.frontLarge}
+            amount={value.amount}
+            size={value.size}
+          />
+          <SingleProductDetails description={props.description} />
+        </div>
+      </div>
+      <AddItemToCartModal
         frontLarge={props.frontLarge}
-        frontSmall={props.frontSmall}
+        size={value.size}
         tshirtName={props.tshirtName}
       />
-      <div className="md:mt-16 lg:mx-12 max-w-sm">
-        <SingleProductHeader
-          tshirtName={props.tshirtName}
-          price={props.price}
-        />
-        <SingleProductSize />
-        <SingleProductQuantity />
-        <SingleProductButton
-          id={props.id}
-          tshirtName={props.tshirtName}
-          leauge={props.leauge}
-          price={props.price}
-          frontLarge={props.frontLarge}
-          amount={value.amount}
-          size={value.size}
-        />
-        <SingleProductDetails description={props.description} />
-      </div>
     </div>
   );
 };
