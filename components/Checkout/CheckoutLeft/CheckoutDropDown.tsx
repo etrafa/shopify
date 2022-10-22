e589e515f11be8c0ea3ba6a5f52189396e2c7e1c
Etem Senel <etemsenel96@hotmail.de>
2022-10-22 07:46:03 +0300

    a bug related to new address fixed.

--- components/Checkout/CheckoutLeft/CheckoutDropDown.tsx
+++ components/Checkout/CheckoutLeft/CheckoutDropDown.tsx
@@ -6,12 +6,34 @@ interface CheckoutDropDownProps {
   setCurrentAddress: Dispatch<SetStateAction<AddressProps>>;
 }
 
+const addressChangeHandler = (
+  e: ChangeEvent<HTMLSelectElement>,
+  setAddress: Dispatch<SetStateAction<AddressProps>>
+) => {
+  if (e.target.value === "new-address") {
+    setAddress({
+      address: "",
+      city: "",
+      country: "",
+      firstName: "",
+      id: "",
+      lastName: "",
+      phone: "",
+      state: "",
+      zipCode: "",
+    });
+  } else {
+    setAddress(JSON.parse(e.target.value));
+  }
+};
+
 const CheckoutDropDown = (props: CheckoutDropDownProps) => {
   return (
     <div>
       <h2 className="text-center mt-12">Shipping Address</h2>
       <select
-        onChange={(e) => props.setCurrentAddress(JSON.parse(e.target.value))}
+        // onChange={(e) => props.setCurrentAddress(JSON.parse(e.target.value))}
+        onChange={(e) => addressChangeHandler(e, props.setCurrentAddress)}
         className="border h-12 border-black pl-2 w-full"
       >
         {props.addresslist.map((address) => (
@@ -19,7 +41,7 @@ const CheckoutDropDown = (props: CheckoutDropDownProps) => {
             {address.address}, {address.city}, {address.country}
           </option>
         ))}
-        <option>Use a new address</option>
+        <option value="new-address">Use a new address</option>
       </select>
     </div>
   );
